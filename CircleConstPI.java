ff02b2701ec6cb213e6bdfec51154e09382220b0
sanghyuk12 <a53051045@gmail.com>
2019-10-15 16:36:12 +0900

    firstDay1

--- CircleConstPI.java
+++ CircleConstPI.java
@@ -1,4 +1,28 @@
+/*
+클래스 내부와 외부에서 참조해야할 정보를 담은 코드
+ */
 package jst;
 
-public class CircleConstPI {
+class Circle {
+    static final double PI = 3.1415;
+    private double radius;
+
+    Circle(double rad) {
+        radius = rad;
+    }
+    void showPerimeter() {
+        double peri = (radius * 2) * PI;
+        System.out.println("둘레: " + peri);
+    }
+    void showArea() {
+        double area = (radius*radius)*PI;
+        System.out.println("넓이: " + area);
+    }
+}
+class CircleConstPI {
+    public static void main(String[] args) {
+        Circle c = new Circle(1.2);
+        c.showPerimeter();
+        c.showArea();
+    }
 }
