f115bd353f5badc3d03d00d330e10c70da9ad2ed
sanghyuk12 <a53051045@gmail.com>
2019-10-15 17:25:38 +0900

    firstDay3

---
+++ CarNBoat.java
@@ -0,0 +1,19 @@
+package jst;
+class Car {
+    void myCar() {
+        System.out.println("This is my car");
+    }
+
+    public static void main(String[] args) {
+        Car c= new Car();
+        c.myCar();
+        Boat t = new Boat();
+        t.myBoat();
+    }
+}
+
+class Boat {
+    void myBoat() {
+        System.out.println("This is my Boat");
+    }
+}
---
+++ StaticImport.java
@@ -0,0 +1,13 @@
+package jst;
+
+import static java.lang.Math.*;//수학 계산을 위한 static import
+
+class StaticImport {
+    public static void main(String[] args) {
+        System.out.println(E);
+        System.out.println(PI);
+        System.out.println(abs(-55));
+        System.out.println(max(77, 88));
+        System.out.println(min(33, 55));
+    }
+}
